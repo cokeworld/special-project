5568dbfef4fea85f12de0c659ce5491c1e1612c9
Tony <xorud1350@naver.com>
2024-07-14 16:29:30 +0900

    추가: 수정 모달 노출 및 데이터 구성
    - todo: 수정 api 구현

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -66,6 +66,7 @@ $(document).ready(function() {
     //     $('#tableBody').append(newRow);
     // });
 
+    // 저장 버튼
     $('#saveDelivery').on('click', function(event) {
         event.preventDefault();
 
@@ -114,7 +115,7 @@ $(document).ready(function() {
                     modalInstance.hide();
                 }
 
-                // 저장 알림 노출
+                // 알림 노출
                 toastElement.find('.toast-body').text('저장 완료');
                 toast.show();
 
@@ -131,7 +132,7 @@ $(document).ready(function() {
 
     // 운송장 조회
     // 현재 CJ대한통운만 구현 완료
-    $('.connect-row').on('click', function() {
+    $('.connectDeliveryButton').on('click', function() {
         const row = $(this).closest('tr');
 
         const deliveryCompanyId = row.find('.deliveryCompanyId').attr('data-deliverycompanyid');
@@ -147,6 +148,86 @@ $(document).ready(function() {
         const query = queryParams.toString();
 
         window.open(`${url}?${query}`, '_blank');
+    });
+
+    // 수정 버튼을 통한 수정모달 열기
+    $('.openDeliveryEditModalButton').on('click', (event) => {
+        const row = $(event.currentTarget).closest('tr');
+
+        const deliveryId = row.find('.deliveryId').text();
+        const deliveryTitle = row.find('.deliveryTitle').text();
+        const deliveryCompanyId = row.find('.deliveryCompanyId').attr('data-deliverycompanyid');
+        const deliveryNumber = row.find('.deliveryNumber').text();
+
+        // 수정 모달 정보 수정
+        const modal = $('#editModal');
+        $('#editDeliveryId').val(deliveryId);
+        $('#editDeliveryTitle').val(deliveryTitle);
+        $('#editDeliveryCompanyId').val(deliveryCompanyId);
+        $('#editDeliveryNumber').val(deliveryNumber);
+
+
+        // const modalInstance = bootstrap.Modal.getInstance(modal);
+        // if (modalInstance) {
+        //     modalInstance.show();
+        // }
+
+    });
+
+    // 수정 모달 내에서 수정 실행
+    $('#editDelivery').on('click', (event) => {
+        event.preventDefault();
+
+        const toastElement = $('#saveToast');
+        let toast = new bootstrap.Toast(toastElement, {
+            delay: 3000
+        });
+
+        // ajax 호출
+        // 데이터 가져오기
+        const modal = $('#editModal');
+
+        const deliveryId = $('#editDeliveryId').val();
+        const deliveryTitle = $('#editDeliveryTitle').val();
+        const deliveryCompanyId = $('#editDeliveryCompanyId').val();
+        const deliveryNumber = $('#editDeliveryNumber').val();
+
+
+        const requestData = {
+            title : deliveryTitle,
+            deliveryCompanyId : deliveryCompanyId,
+            deliveryNumber : deliveryNumber,
+            status : 1,
+            memberId: 1
+        };
+
+        // TODO: promise로 변경
+        $.ajax({
+            url: `/api/v1/delivery/${deliveryId}`,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(requestData),
+            success: function(response) {
+                // 모달 닫기
+                const modal = $('#saveModal');
+                const modalInstance = bootstrap.Modal.getInstance(modal);
+                if (modalInstance) {
+                    modalInstance.hide();
+                }
+
+                // 토스트 노출
+                toastElement.find('.toast-body').text('수정 완료');
+                toast.show();
+
+                // 택배 목록 갱신
+            },
+            error: function(error) {
+                toastElement.find('.toast-body').text('수정 실패. 다시 시도해주세요.');
+                toast.show();
+
+                console.log(error);
+            }
+        });
     });
 
 });
